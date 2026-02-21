9456749626c72f8ade73c0799c2aed51cb6bdf1f
rico.carbonel@motivit.com <rico.carbonel@motivit.com>
2026-02-22 01:42:02 +0800

    update port

--- Program.cs
+++ Program.cs
@@ -44,7 +44,8 @@ if (!string.IsNullOrEmpty(databaseUrl))
     // Convert Render's postgres:// format to Npgsql format
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
+    var port = uri.Port <= 0 ? 5432 : uri.Port;
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
 }
 else
 {
